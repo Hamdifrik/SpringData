165a87690d95d4fe7270005ae55e242a80cee173
hamdifrik <hamdifrik01@gmail.com>
2023-08-07 00:13:44 +0100

    connexion base de donner

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,4 @@ insert into employees(first_name,last_name,salary)values('ahmed','mohsen','1000'
 insert into employees(first_name,last_name,salary)values('hamdi','frik','2000');
 insert into employees(first_name,last_name,salary)values('jamel','belmadhi','3000');
 insert into employees(first_name,last_name,salary)values('youssef','missaoui','4000');
+
